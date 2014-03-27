0bd4a127f160f107a1f73e5f0bd15499fbbc411f
Matt Thomas <matt@betweenbrain.com>
2014-03-27 15:21:32 -0400

    Working POC

--- ajax_modal.php
+++ ajax_modal.php
@@ -15,20 +15,90 @@ jimport('joomla.plugin.plugin');
 class plgAjaxAjax_modal extends JPlugin
 {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param   object &$subject The object to observe
+	 * @param   array  $config   An optional associative array of configuration settings.
+	 *
+	 * @since   0.1
+	 */
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		$this->app = JFactory::getApplication();
+		$this->db  = JFactory::getDbo();
+
+	}
+
 	function onAjaxAjax_modal()
 	{
 
-		$response = "
+		$function  = $this->app->input->getCmd('function', 'jSelectArticle');
+		$component = $this->app->input->getCmd('component', 'content');
+
+		switch ($component)
+		{
+			case('content'):
+				$query = $this->db->getQuery(true);
+				$query
+					->select('*')
+					->from($this->db->quoteName('#__content'))
+					->where($this->db->quoteName('state') . ' = ' . $this->db->quote('1'));
+
+				$this->db->setQuery($query);
+
+				$this->items = $this->db->loadObjectList();
+
+				require_once JPATH_ROOT . '/components/com_content/helpers/route.php';
+
+				break;
+		}
+
+		ob_start(); ?>
+
 		<!DOCTYPE html>
 		<html>
 		<head>
 			<title></title>
 		</head>
 		<body>
-		<h1>Hello Mom!</h1>
+		<table class="table table-striped table-condensed">
+			<thead>
+			<tbody>
+			<?php foreach ($this->items as $i => $item) : ?>
+				<tr class="row<?php echo $i % 2; ?>">
+					<td>
+						<a href="javascript:void(0)" onclick="if (window.parent) window.parent.<?php echo htmlspecialchars($function); ?>('<?php echo $item->id; ?>', '<?php echo htmlspecialchars(addslashes($item->title)); ?>', '<?php echo htmlspecialchars($item->catid); ?>', null, '<?php echo htmlspecialchars(ContentHelperRoute::getArticleRoute($item->id, $item->catid, $item->language)); ?>', '', null);">
+							<?php echo htmlspecialchars($item->title); ?></a>
+					</td>
+					<td class="center">
+						<?php echo htmlspecialchars($item->access); ?>
+					</td>
+					<td class="center">
+						<?php echo htmlspecialchars($item->catid); ?>
+					</td>
+					<td class="center">
+						<?php if ($item->language == '*'): ?>
+							<?php echo JText::alt('JALL', 'language'); ?>
+						<?php else: ?>
+							<?php echo $item->language_title ? htmlspecialchars($item->language_title) : JText::_('JUNDEFINED'); ?>
+						<?php endif; ?>
+					</td>
+					<td class="center nowrap">
+						<?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+					</td>
+					<td class="center">
+						<?php echo (int) $item->id; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
 		</body>
-		</html>";
+		</html>
 
-		return $response;
+		<?php return ob_get_clean();
 	}
 }
