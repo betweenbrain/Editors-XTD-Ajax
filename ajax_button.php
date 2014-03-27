137ccf96a9c28435f69a34adcdceb0f6a36a1e83
Matt Thomas <matt@betweenbrain.com>
2014-03-27 14:27:33 -0400

    Seed editors-xtd-develop

---
+++ ajax_button.php
@@ -0,0 +1,12 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       ajax_button.php
+ * Created    3/27/14 2:27 PM 
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+ 
+ 
